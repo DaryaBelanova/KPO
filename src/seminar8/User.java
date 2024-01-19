d2251ef684d4bf2a73375eeb49f453b79801e68c
Darya Belanova <dsbelanova@edu.hse.ru>
2024-01-19 16:53:13 +0300

    Seminar9 19/01/2024

---
+++ src/seminar8/User.java
@@ -0,0 +1,10 @@
+package seminar8;
+
+public class User {
+    public String name;
+    public String imageURL;
+    public String storeName;
+    public int maxNumberItems;
+    public int creditCardNumber;
+    public Role role;
+}
